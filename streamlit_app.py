00e44f7f4f0f9991e97890f7796157f18e705dc7
mateobr99 <154276229+mateobr99@users.noreply.github.com>
2025-02-11 16:11:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ st.image("boxplot_Humidity_pct.png")
 st.image("boxplot_Wind_Speed_kmh.png")
 
 def load_model():
-    filename = "best_model (2).pkl.gz"
+    filename = "best_model (3).pkl.gz"
     with gzip.open(filename, 'rb') as f:
         model = pickle.load(f)
     return model
@@ -30,13 +30,19 @@ model = load_model()
 
 st.title("Predicción de Temperatura")
 
+cities_order = ['San Diego', 'Philadelphia', 'San Antonio', 'San Jose', 'New York',
+                'Houston', 'Dallas', 'Chicago', 'Los Angeles', 'Phoenix']
+
 
 humedad = st.slider("Humedad (%)", min_value=0, max_value=100, value=50)
 viento = st.slider("Viento (km/h)", min_value=0.0, max_value=50.0, value=10.0)
 precipitaciones = st.slider("Precipitación (mm)", min_value=0.0, max_value=100.0, value=5.0)
 
-input_data = np.array([[humedad, viento, precipitaciones]])
+# Crear el vector one-hot para la ciudad seleccionada
+selected_city_vector = [1 if city == selected_city else 0 for city in cities_order]
 
+# Formar el vector de entrada completo (asegúrate de respetar el orden usado en el entrenamiento)
+input_data = np.array([[humedad, viento, precipitaciones] + selected_city_vector])
 
 if st.button("Predecir Temperatura"):
     prediccion = model.predict(input_data)
