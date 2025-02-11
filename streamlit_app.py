8664e8cea349287faa76f9b7a659defe07f8adb0
mateobr99 <154276229+mateobr99@users.noreply.github.com>
2025-02-11 16:13:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,8 @@ st.title("Predicción de Temperatura")
 cities_order = ['San Diego', 'Philadelphia', 'San Antonio', 'San Jose', 'New York',
                 'Houston', 'Dallas', 'Chicago', 'Los Angeles', 'Phoenix']
 
+# Mostrar un select box para elegir la ciudad
+selected_city = st.selectbox("Selecciona la Ciudad", cities_order)
 
 humedad = st.slider("Humedad (%)", min_value=0, max_value=100, value=50)
 viento = st.slider("Viento (km/h)", min_value=0.0, max_value=50.0, value=10.0)
@@ -40,6 +42,7 @@ precipitaciones = st.slider("Precipitación (mm)", min_value=0.0, max_value=100.
 
 # Crear el vector one-hot para la ciudad seleccionada
 selected_city_vector = [1 if city == selected_city else 0 for city in cities_order]
+st.write("Vector one-hot:", selected_city_vector)
 
 # Formar el vector de entrada completo (asegúrate de respetar el orden usado en el entrenamiento)
 input_data = np.array([[humedad, viento, precipitaciones] + selected_city_vector])
